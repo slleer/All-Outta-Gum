3477e0d01afb6a5897dc4cd977a4f34c2a7e8642
DesertPangolin <michaelpstepzinski@gmail.com>
2021-04-06 23:49:51 -0700

    added game map and basic player with controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //float deltaPosition = 5.0f;
+    //Vector3 position;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(10 * Vector3.forward * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(10 * Vector3.back * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(10 * Vector3.left * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(10 * Vector3.right * Time.deltaTime);
+
+        //transform.Translate(position);
+    }
+}
