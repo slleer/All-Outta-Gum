797e5998f3d3f70ed81b57b1a05e7cdc4c92885d
DesertPangolin <michaelpstepzinski@gmail.com>
2021-04-08 00:06:15 -0700

    proper object rotation and camera movement

---
+++ Assets/CameraLookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAtPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public GameObject target;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, 40, target.transform.position.z);
+        //transform.position.z = target.transform.position.z;
+    }
+}
---
+++ Assets/RotateYTowardsMouse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateYTowardsMouse : MonoBehaviour
+{
+    public Vector2 screenDimensions;
+    public Vector2 screenCenter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }    
+
+    public Vector2 mousePosition;
+    public float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenDimensions.x = Screen.width;
+        screenDimensions.y = Screen.height;
+        screenCenter.x = screenDimensions.x / 2;
+        screenCenter.y = screenDimensions.y / 2;
+        mousePosition.x = Input.mousePosition.x;
+        mousePosition.y = Input.mousePosition.y;
+
+        float angleX = mousePosition.x - screenCenter.x;
+        float angleY = mousePosition.y - screenCenter.y;
+      
+        angle = Mathf.Atan(angleY / angleX) * Mathf.Rad2Deg;
+        //angle += 90.0f;
+        //if (angle < 0)
+        //    angle += 180.0f;
+        //if (angle > 180)
+        //    angle += 180.0f;
+        if (angleX < 0)
+            angle += 180;
+        angle += 180;
+
+        transform.eulerAngles = new Vector3(0, -angle, 0);
+    }
+}
