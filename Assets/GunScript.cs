c8d4d539558d735cdbfef8a7a686a3ab2b81bfcf
DesertPangolin <michaelpstepzinski@gmail.com>
2021-04-08 20:43:36 -0700

    added basic weapon system

---
+++ Assets/GunScript.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10.0f;
+    public float range = 100.0f;
+    public float impactForce = 30.0f;
+
+    public GameObject weapon;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetButtonDown("Fire1"))
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("left click");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        
+        //muzzleFlash.Stop();
+        RaycastHit hit;
+        if(Physics.Raycast(weapon.transform.position, weapon.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+
+        GameObject impactInstant = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Object.Destroy(impactInstant, 2f);
+        //muzzleFlash.Stop();
+    }
+}
