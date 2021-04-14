5b6cb4e764610c08f174d7e9b2599ccf014017a3
DesertPangolin <michaelpstepzinski@gmail.com>
2021-04-14 14:08:58 -0700

    added shooting sounds

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -6,18 +6,26 @@ public class GunScript : MonoBehaviour
     public float damage = 10.0f;
     public float range = 100.0f;
     public float impactForce = 30.0f;
+    public float fireRate = 1.0f;
 
     public GameObject weapon;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
+    public AudioSource audioSource;
+
+    private float nextTimeToFire = 0.0f;
     // Update is called once per frame
     void Update()
     {
         //if(Input.GetButtonDown("Fire1"))
-        
-        if(Input.GetMouseButtonDown(0))
+
+        if(Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
+        //if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("left click");
+            //Debug.Log("left click");
+            //nextTimeToFire = Time.time + 1f / fireRate;
+            //nextTimeToFire = Time.time + fireRate;
+            nextTimeToFire = Time.time + 0.5f;
             Shoot();
         }
     }
@@ -25,7 +33,7 @@ public class GunScript : MonoBehaviour
     void Shoot()
     {
         muzzleFlash.Play();
-        
+        audioSource.Play();
         //muzzleFlash.Stop();
         RaycastHit hit;
         if(Physics.Raycast(weapon.transform.position, weapon.transform.forward, out hit))
--- Assets/RotateYTowardsMouse.cs
+++ Assets/RotateYTowardsMouse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateYTowardsMouse : MonoBehaviour
 {
-    public Vector2 screenDimensions;
+    //public Vector2 screenDimensions;
     public Vector2 screenCenter;
     // Start is called before the first frame update
     void Start()
@@ -12,23 +12,29 @@ public class RotateYTowardsMouse : MonoBehaviour
         
     }    
 
-    public Vector2 mousePosition;
+    //public Vector2 mousePosition;
     public float angle;
 
     // Update is called once per frame
     void Update()
     {
-        screenDimensions.x = Screen.width;
-        screenDimensions.y = Screen.height;
-        screenCenter.x = screenDimensions.x / 2;
-        screenCenter.y = screenDimensions.y / 2;
-        mousePosition.x = Input.mousePosition.x;
-        mousePosition.y = Input.mousePosition.y;
+        //screenDimensions.x = Screen.width;
+        //screenDimensions.y = Screen.height;
+        //screenCenter.x = screenDimensions.x / 2;
+        //screenCenter.y = screenDimensions.y / 2;
+        //screenCenter.x = Screen.width / 2;
+        //screenCenter.y = Screen.height / 2;
+        //mousePosition.x = Input.mousePosition.x;
+        //mousePosition.y = Input.mousePosition.y;
 
-        float angleX = mousePosition.x - screenCenter.x;
-        float angleY = mousePosition.y - screenCenter.y;
-      
-        angle = Mathf.Atan(angleY / angleX) * Mathf.Rad2Deg;
+        //float angleX = mousePosition.x - screenCenter.x;
+        //float angleY = mousePosition.y - screenCenter.y;
+
+        //angle = Mathf.Atan(angleY / angleX) * Mathf.Rad2Deg;
+
+        float angleX = Input.mousePosition.x - (Screen.width / 2);
+
+        angle = Mathf.Atan((Input.mousePosition.y - (Screen.height / 2)) / angleX) * Mathf.Rad2Deg;
         //angle += 90.0f;
         //if (angle < 0)
         //    angle += 180.0f;
