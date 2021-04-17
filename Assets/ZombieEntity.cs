e16052b3395023918015e3ded1a649d7c7f5493c
DesertPangolin <michaelpstepzinski@gmail.com>
2021-04-17 15:14:05 -0700

    added some scripts

---
+++ Assets/ZombieEntity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieEntity : MonoBehaviour
+{
+    public Vector3 desiredPosition;
+    public float desiredHeading;
+    public float speed;
+    public float turningSpeed;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 8.0f;
+        turningSpeed = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        desiredPosition = player.transform.position;
+        desiredHeading = CalculateAngle(desiredPosition, transform.position);
+        while (desiredHeading < 0)
+        {
+            desiredHeading += 360;
+        }
+        eulerRotation.y = desiredHeading * turningSpeed;
+        transform.localEulerAngles = eulerRotation;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    public Vector3 eulerRotation = Vector3.zero;
+
+    float CalculateAngle(Vector3 moveTo, Vector3 currentPosition)
+    {
+        Vector3 diff = moveTo - currentPosition;
+        return -(Mathf.Atan2(diff.z, diff.x) * Mathf.Rad2Deg + 270);
+    }
+}
---
+++ Assets/ZombieManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : MonoBehaviour
+{
+    public static ZombieManager inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawnPoint = new Vector3(-13.0f, 2.5f, -40.0f);
+        //zombieTransform.
+    }
+
+    public float spawnTimer = 3.0f;
+    public GameObject regularZombie;
+    public Transform zombieTransform;
+    //public Vector3 spawnPoint;
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn zombies every 3 seconds
+        if (spawnTimer > 0)
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+        else
+        {
+            spawnTimer = 3.0f;
+            //instantiate zombie
+            //GameObject impactInstant = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            //GameObject zombieInstant = Instantiate(regularZombie, spawnPoint);
+        }
+    }
+}
